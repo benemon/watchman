9fa32bee00119dd0871c1cfe497861adc8533a77
Benjamin Holmes <benjamin.holmes@gmail.com>
2017-12-16 11:09:53 +0000

    externalised config

--- src/main/java/com/redhat/ukiservices/openshift/watchman/Watchman.java
+++ src/main/java/com/redhat/ukiservices/openshift/watchman/Watchman.java
@@ -17,11 +17,18 @@ import java.util.logging.Logger;
 public class Watchman {
     private static final Logger logger = Logger.getLogger(Watchman.class.getName());
 
+    private static final String OPENSHIFT_WEBSOCKET_URI_ENV = "OPENSHIFT_WEBSOCKET_URI";
+    private static final String OPENSHIFT_WEBSOCKET_TIMEOUT_ENV = "OPENSHIFT_WEBSOCKET_TIMEOUT";
+    private static final String OPENSHIFT_WEBSOCKET_URI_DEFAULT = "wss://192.168.99.100:8443/oapi/v1/deploymentconfigs?watch=true";
+    private static final String OPENSHIFT_WEBSOCKET_TIMEOUT_DEFAULT = "600000";
+
     public static void main(String[] args) {
         try {
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
 
-            String uri = "wss://192.168.99.100:8443/oapi/v1/deploymentconfigs?watch=true";
+            String uri = System.getenv(OPENSHIFT_WEBSOCKET_URI_ENV) != null ? System.getenv(OPENSHIFT_WEBSOCKET_URI_ENV) : OPENSHIFT_WEBSOCKET_URI_DEFAULT;
+            Long timeout = Long.parseLong(System.getenv(OPENSHIFT_WEBSOCKET_TIMEOUT_ENV) != null ? System.getenv(OPENSHIFT_WEBSOCKET_TIMEOUT_ENV) : OPENSHIFT_WEBSOCKET_TIMEOUT_DEFAULT);
+
             logger.info("Connecting to " + uri);
 
             // Create a custom Configuration object to hold our Bearer token
@@ -34,7 +41,7 @@ public class Watchman {
             config.getUserProperties().put(DefaultWebSocketClientSslProvider.SSL_CONTEXT, sslContext);
 
             Session session = container.connectToServer(new OpenShiftClientEndpoint(), config, URI.create(uri));
-            session.setMaxIdleTimeout(600000);
+            session.setMaxIdleTimeout(timeout);
             session.addMessageHandler(new OpenShiftDeploymentConfigMessageHandler());
 
             while (session.isOpen()) {
--- src/main/java/com/redhat/ukiservices/openshift/watchman/configuration/OpenShiftClientEndpointConfig.java
+++ src/main/java/com/redhat/ukiservices/openshift/watchman/configuration/OpenShiftClientEndpointConfig.java
@@ -4,11 +4,24 @@ import javax.websocket.ClientEndpointConfig;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class OpenShiftClientEndpointConfig extends ClientEndpointConfig.Configurator {
 
+    private static Logger logger = Logger.getLogger(OpenShiftClientEndpointConfig.class.getName());
+
+    private static final String OPENSHIFT_WEBSOCKET_TOKEN_ENV = "OPENSHIFT_WEBSOCKET_TOKEN";
+
     @Override
     public void beforeRequest(Map<String, List<String>> headers) {
-        headers.put("Authorization", Arrays.asList("Bearer 2wdr4QFgDTHZqh_eb9waIrPeYUS7IBhJ3QOwS7qhZmY"));
+
+        String token = System.getenv(OPENSHIFT_WEBSOCKET_TOKEN_ENV);
+
+        if (token != null && token.length() > 0) {
+            headers.put("Authorization", Arrays.asList(String.format("Bearer %s", token)));
+        } else {
+            logger.warning("No Authorization token found.");
+        }
+
     }
 }
